a54f5b523afe38ebd1698f45921898cfd4da7dcc
Priyadharshini-086 <151605586+Priyadharshini-086@users.noreply.github.com>
2025-08-04 22:38:48 +0530

    Add files via upload

---
+++ MahoutRecommendationSystem/src/com/recommendation/Main.java
@@ -0,0 +1,36 @@
+package com.recommendation;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            DataModel model = new FileDataModel(new File("data/dataset.csv"));
+            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+            UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+            UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+            int userId = 1;
+            List<RecommendedItem> recommendations = recommender.recommend(userId, 2);
+            for (RecommendedItem recommendation : recommendations) {
+                System.out.println("Recommended Item: " + recommendation.getItemID() + " Value: " + recommendation.getValue());
+            }
+
+        } catch (IOException | TasteException e) {
+            e.printStackTrace();
+        }
+    }
+}
